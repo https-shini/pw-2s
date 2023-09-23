53f234930835a1e5910d85b89826550c8aeb77c1
Guilherme Souza <100307080+https-shini@users.noreply.github.com>
2023-09-23 17:01:58 -0300

    Add files via upload

---
+++ Aula06/Exemplos/exemplo01/js/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let x = 4.55845;
+let y = 4;
+let z = Math.random() * 1000;
+
+console.log(x/y);
+console.log(z);
+console.log(Math.ceil(x/y));
+console.log(Math.floor(x/y));
+console.log(Math.round(x/y));
+console.log(Math.round(z));
+console.log(z.toFixed(2));
+let w = z.toFixed(2);
+console.log(Number(w)+2);
