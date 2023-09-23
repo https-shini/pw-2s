fc4d0c73e8412ee3b9dc369c7eaaca1db41907e5
Guilherme Souza <100307080+https-shini@users.noreply.github.com>
2023-09-23 18:49:11 -0300

    Add files via upload

---
+++ Aula04/Exercicios/exercicio01/js/script.js
@@ -0,0 +1,10 @@
+//Como calcuar média ponderada com JS.
+'use strict'
+
+let n1 = parseFloat(prompt("Digite N1: "));
+let n2 = parseFloat(prompt("Digite N2: "));
+let n3 = parseFloat(prompt("Digite N3: "));
+let n4 = parseFloat(prompt("Digite N4: "));
+let media = (n1+n2+n3+n4)/4;
+
+alert(`A sua média é \n${media}`);
